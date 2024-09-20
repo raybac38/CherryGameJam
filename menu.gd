a8cede5dc6b43d0ac57a2d5fa4ac19efbc2f8082
The chosen one <romaingueville378@gmail.com>
2024-09-20 19:13:17 +0200

    menu principal

---
+++ menu.gd
@@ -0,0 +1,19 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func open_settings() -> void:
+	hide()
+	
+	
+func close_settings() -> void:
+	show()
