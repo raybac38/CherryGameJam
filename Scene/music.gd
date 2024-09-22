bb6946d11b90d3040f11d043d2e45df3c404da0c
Ralpha230 <49341011+Ralpha230@users.noreply.github.com>
2024-09-22 18:31:20 +0200

    Implémentation manager musique
    Implémentation des fonctions play_ambient et play_combat avec système de looping + changement vers le format OGG

--- Scene/music.gd
+++ Scene/music.gd
@@ -1,19 +1,43 @@
 extends AudioStreamPlayer
 
-#TODO faire loop la musique
+#TODO comprendre pourquoi les attributs du stream sont grisés
 
 func play_ambient():
-	var sound = load("res://Assets/Son/Musique/ost_chill.mp3")
+	var sound = load("res://Assets/Son/Musique/ost_chill.ogg")
 	if stream == sound:
 		return
 	if playing:
 		stop()
 		stream = sound
 		play()
+		stream.loop = true
+		stream.loop_offset = 33.88
 		return
 	return
 
-#TODO play_combat(phase)
+func play_combat(phase: int):
+	var sound
+	if phase == 1:
+		sound = load("res://Assets/Son/Musique/ost_fight1.ogg")
+	elif phase == 2:
+		sound = load("res://Assets/Son/Musique/ost_fight2.ogg")
+	elif phase == 3:
+		sound = load("res://Assets/Son/Musique/ost_fight3.ogg")
+	elif phase == 4:
+		sound = load("res://Assets/Son/Musique/ost_fight4.ogg")
+	else:
+		return
+		
+	if stream == sound:
+		return
+	if playing:
+		stop()
+		stream = sound
+		play()
+		stream.loop = true
+		stream.loop_offset = 16.94
+		return
+	return
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
